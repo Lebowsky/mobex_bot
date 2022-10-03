f54e843edf055370b9669c1b737d360c713b158c
And <ase@crimea.com>
2022-10-03 17:41:05 +0300

    add gino api

--- data/config.py
+++ data/config.py
@@ -8,3 +8,10 @@ TOKEN = os.getenv('TOKEN')
 admins_id = [
     440206915
 ]
+
+ip = os.getenv('ip')
+PGUSER = os.getenv('PGUSER')
+PGPASSWORD = os.getenv('PGPASSWORD')
+DATABASE = os.getenv('DATABASE')
+
+POSTGRES_URI = f'postgresql://{PGUSER}:{PGPASSWORD}@{ip}/{DATABASE}'
